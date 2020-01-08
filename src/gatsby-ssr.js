c43e3bb3342180dbba99bab357c9c0e43e8b910e
GeraldM <gerald@bejamas.io>
2020-01-07 23:06:51 -0600

    Add initial useHttpHeader option
    This feature is only for netlify config file
    - Validate if user choise http header instead meta
    - Create files and insert rules

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,11 +1,12 @@
 const React = require("react");
 const flatten = require("lodash.flatten");
+var fs = require("fs");
 
 const {
   computeHash,
   cspString,
   getHashes,
-  defaultDirectives
+  defaultDirectives,
 } = require("./utils");
 
 exports.onPreRenderHTML = (
@@ -13,9 +14,9 @@ exports.onPreRenderHTML = (
     getHeadComponents,
     replaceHeadComponents,
     getPreBodyComponents,
-    getPostBodyComponents
+    getPostBodyComponents,
   },
-  userPluginOptions
+  userPluginOptions,
 ) => {
   const {
     disableOnDev = true,
@@ -23,7 +24,8 @@ exports.onPreRenderHTML = (
     mergeScriptHashes = true,
     mergeStyleHashes = true,
     mergeDefaultDirectives = true,
-    directives: userDirectives
+    useHttpHeader = false,
+    directives: userDirectives,
   } = userPluginOptions;
 
   // early return if plugin is disabled on dev env
@@ -34,12 +36,12 @@ exports.onPreRenderHTML = (
   let components = [
     ...flatten(getHeadComponents()),
     ...flatten(getPostBodyComponents()),
-    ...flatten(getPreBodyComponents())
+    ...flatten(getPreBodyComponents()),
   ];
 
   let directives = {
     ...(mergeDefaultDirectives && defaultDirectives),
-    ...userDirectives
+    ...userDirectives,
   };
 
   let csp = {
@@ -47,23 +49,63 @@ exports.onPreRenderHTML = (
     ...(mergeScriptHashes && {
       "script-src": `${directives["script-src"] || ""} ${getHashes(
         components,
-        "script"
-      )}`
+        "script",
+      )}`,
     }),
     ...(mergeStyleHashes && {
       "style-src": `${directives["style-src"] || ""} ${getHashes(
         components,
-        "style"
-      )}`
-    })
+        "style",
+      )}`,
+    }),
   };
 
-  const cspComponent = React.createElement("meta", {
-    httpEquiv: `${reportOnly ? "Content-Security-Policy-Report-Only" : "Content-Security-Policy"}`,
-    content: cspString(csp)
-  });
+  if (useHttpHeader) {
+    //Add http header
+    const path = "_headers";
+    const httpEquiv = reportOnly
+      ? "Content-Security-Policy-Report-Only"
+      : "Content-Security-Policy";
+    const rules = cspString(csp);
+    const finalText = `# csp rules\n/*\n ${httpEquiv}: ${rules}\n# csp rules`;
 
-  let headComponentsWithCsp = [cspComponent, ...getHeadComponents()];
+    try {
+      if (fs.existsSync(path)) {
+        //file exists
+        console.log("Already exist _headers file, will be replace config");
+        fs.writeFile(path, finalText, function(err) {
+          if (err) {
+            return console.log(err);
+          }
+        });
+      } else {
+        const writeStream = fs.createWriteStream("_headers");
 
-  replaceHeadComponents(headComponentsWithCsp);
+        writeStream.write(finalText);
+
+        writeStream.on("finish", () => {
+          console.log("Rules writen successfull");
+        });
+
+        // close the stream
+        writeStream.end();
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    //Add Meta
+    const cspComponent = React.createElement("meta", {
+      httpEquiv: `${
+        reportOnly
+          ? "Content-Security-Policy-Report-Only"
+          : "Content-Security-Policy"
+      }`,
+      content: cspString(csp),
+    });
+
+    let headComponentsWithCsp = [cspComponent, ...getHeadComponents()];
+
+    replaceHeadComponents(headComponentsWithCsp);
+  }
 };
